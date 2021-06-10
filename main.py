6b6f96ccba1c53af4c3d7c33a51a49aa32a442bf
Inzi-Jack <75156244+Inzi-Jack@users.noreply.github.com>
2021-06-10 05:43:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
+i# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 06:47:24 2021
+
+@author: Jack
+"""
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
 import csv
 
-
-#################################################################
-def main():
-
-    with open('data.csv') as file:
-        readerobject = csv.reader(file, delimiter=',',)
-
-        count = 0
-
-        # For every row in file
-        for row in readerobject:
-
-            # If we're on row 0, the headder row, just print the headder names, do not process.
-            if count == 0:
-                count += 1  # increase the count before exiting loop, so that we don't get stuck here forever.
-                pass
-                # print(row[0], row[1], row[2], row[3])
-
-            # For all rows other than the headder row:
-            else:
-
-                # If column 4 value > 0.2, print some entries from that row
-                if float(row[3]) > 0.2:
-                    print("row # ", count, "       ", row[0], row[1], row[2], row[3], row[4], row[5])
-
-                # if count > 4:
-                #     break
-
-                count += 1
+def header(msg):
+    print('-' * 50)
+    print('[ ' + msg + ' ]')
 
 
-#################################################################
-if __name__ == "__main__":
-    main()
-    
-    
+# 2. read text file into a dataframe
+header("2. read text file into a df")
+fn = "DATALOG2.csv"
+col_names=['Date Time', 'Roll', 'Pitch', 'Direction', 'Temperature'] 
+df = pd.read_csv('DATALOG2.csv', names=col_names)
+#df = pd.read_csv(fn)
+print(df)
+
+
+# 3. Print first 5 or last 3 rows
+#header("3. df.head()")
+#print(df.head())
+#header("3. df.tail(3)")
+#print(df.tail(3))
+
+frame2 = pd.DataFrame(df,columns=['Date Time','Roll','Pitch'])
+print (frame2)
+
+frame3 = pd.DataFrame(df,columns=['Roll'])
+
+#frame2.loc[1620577]
+#frame2.T
+#print(frame2) 
+
+for i in frame3.T:                #pgs. 47, 48 in 2018 Wes McKinny book
+    if i >= 2.2:
+        print('Found i')
+        print(['Roll'])
+        print (i)
+        break
+    else:
+        print ('Not Found')
