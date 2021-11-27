8c2a3e23c889d8c845d25b5cf58d8bda7c1fdb12
Tomasz Tudrej <ttudrej@gmail.com>
2021-11-27 15:23:33 -0500

    writing data frame into spreadsheet

--- jackv6A_2.py
+++ jackv6A_2.py
@@ -17,6 +17,7 @@ import numpy as np
 from openpyxl import load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 
+
 # wb = load_workbook('D:/DataFileForSeismometer/Seismograph_Data.xlsx')
 wb = load_workbook('./Seismograph_Data.xlsx')
 ws = wb.active
@@ -25,15 +26,17 @@ col_names=['Date Time', 'Roll', 'Pitch', 'Direction Angle', 'Temperature']
 # df = pd.read_csv('D:/DataFileForSeismometer/DATALOG2.csv', names=col_names, low_memory=False, encoding='utf-8', dtype={"Data Time": str, "Roll": str, "Pitch": str, "Direction Angle": str, "Temperature": str})
 df = pd.read_csv('./DATALOG1.csv', names=col_names, low_memory=False, encoding='utf-8', dtype={"Data Time": str, "Roll": str, "Pitch": str, "Direction Angle": str, "Temperature": str})
 
-#print(df)
+# print(df)
+# exit()
 
 #col_names=['Date Time', 'Roll', 'Pitch', 'Direction Angle', 'Temperature']
 #df = pd.read_csv ('D:/DataFileForSeismometer/DATALOG2.csv', low_memory=False, names=col_names)
 #df_1 = pd.DataFrame(df,columns=['Date Time', 'Roll', 'Pitch'])
 
-df_1= df["Roll"]
+# df_1= df["Roll"]
 
-#print(df_2)
+# print(df_1)
+# exit()
 
 #print(df_2.abs())
 #df_1 = df_1.replace(np.nan, 'N/A', regex=True)
@@ -44,12 +47,23 @@ df_1= df["Roll"]
 #print(df2)
 
 #df2.drop(columns='N/A', inplace=True)
-rows = dataframe_to_rows(df_1)
-for r_idx, row in enumerate(rows,2):
-   for c_idx, value in enumerate (row,1):
-      ws.cell(row=r_idx, column=c_idx, value=value)
 
-df1['Roll'].abs()
 
-print(df1['Roll'].abs())
+# rows = dataframe_to_rows(df_1)
+
+# for r_idx, row in enumerate(rows,2):
+#    for c_idx, value in enumerate (row,1):
+#       ws.cell(row=r_idx, column=c_idx, value=value)
+
+# df1['Roll'].abs()
+# print(df1['Roll'].abs())
 #wb.save('Seismograph_Data2.xlsx')
+
+
+
+for row in dataframe_to_rows(df, index=False, header=True):
+    ws.append(row)
+   #  print(row)
+   #  exit()
+
+wb.save('Seismograph_Data.xlsx')
