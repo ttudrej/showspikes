2a2357de23c8597a81fb192aebe11305eddc44f5
Tomasz Tudrej <ttudrej@gmail.com>
2021-12-02 08:30:37 -0500

    Creates new wb, gitignore - removes xlsx files.

--- jackv6A_2.py
+++ jackv6A_2.py
@@ -7,13 +7,23 @@ Created on Tue Nov 16 18:40:36 2021
 
 import pandas as pd
 import numpy as np
-from openpyxl import load_workbook
+# from openpyxl import load_workbook
+from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-
+# Open an existing workbook:
 # wb = load_workbook('D:/DataFileForSeismometer/Seismograph_Data.xlsx')
-wb = load_workbook('./Seismograph_Data.xlsx')
+# wb = load_workbook('./Seismograph_Data.xlsx')
+
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html
+# Crate a new notebook
+wb = Workbook()
+# A workbook is always created with at least one worksheet. You can get it by using the Workbook.active property:
 ws = wb.active
+# Name worksheet 0
+ws.title = "My seimic data"
+
+# exit()
 
 # https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
 # dtypeType name or dict of column -> type, optional
@@ -29,11 +39,13 @@ print(df)
 df['Roll'] = df['Roll'].abs()
 df['Pitch'] = df['Pitch'].abs()
 
-print(df)
+# Print elements 1 to 5 only.
+print(df.iloc[1:5])
 
 # To convert a dataframe into a worksheet
 # https://openpyxl.readthedocs.io/en/stable/pandas.html
 for row in dataframe_to_rows(df, index=False, header=True):
     ws.append(row)
 
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html#saving-to-a-file
 wb.save('Seismograph_Data.xlsx')
