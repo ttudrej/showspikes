b9a8e98fa46c327e43dc8f0d97130bb288a1c1ca
Inzi-Jack <75156244+Inzi-Jack@users.noreply.github.com>
2021-12-03 17:30:31 +0100

    Create jackv6A_3.py
    See the comment on line 35.
    Outpu follows. please run it on you system and verify.
    
     Date Time   Roll   Pitch Direction Angle  Temperature
    2021/1/29   10:53:0"  -0.03       0           62.66        21.23
    2021/1/29   10:53:1"  -0.08    0.01           45.12        21.23
    2021/1/29   10:53:1"   -0.1    0.01           44.76        21.24
    2021/1/29   10:53:2"  -0.12    0.01            63.2        21.23
    2021/1/29   10:53:3"  -0.16    0.02           64.51        21.24
                 ...    ...     ...             ...          ...
    2021/3/29  7:14:44",  0.03,  -0.20,          98.98,        22.73
    2021/3/29  7:14:45",  0.03,  -0.20,         102.63,        22.73
    2021/3/29  7:14:46",  0.03,  -0.20,         102.38,        22.72
    2021/3/29  7:14:47",  0.03,  -0.20,         108.94,        22.72
    2021/3/29  7:14:48",  0.03,  -0.20,          96.99,        22.73
    
    [57308 rows x 5 columns]
    Traceback (most recent call last):
    
      File "C:\Users\Jack\AppData\Local\Temp/ipykernel_6252/3359667898.py", line 1, in <module>
        runfile('C:/Users/Jack/Desktop/Certificates from LinkedIn Courses/Ex_Files_Python_EssT/Ex_Files_Python_EssT/Exercise Files/Chap03/jackv6A_2.py', wdir='C:/Users/Jack/Desktop/Certificates from LinkedIn Courses/Ex_Files_Python_EssT/Ex_Files_Python_EssT/Exercise Files/Chap03')
    
      File "C:\Users\Jack\AppData\Local\Programs\Python\Python39\lib\site-packages\debugpy\_vendored\pydevd\_pydev_bundle\pydev_umd.py", line 167, in runfile
        execfile(filename, namespace)
    
      File "C:\Users\Jack\AppData\Local\Programs\Python\Python39\lib\site-packages\debugpy\_vendored\pydevd\_pydev_imps\_pydev_execfile.py", line 25, in execfile
        exec(compile(contents + "\n", file, 'exec'), glob, loc)
    
      File "C:/Users/Jack/Desktop/Certificates from LinkedIn Courses/Ex_Files_Python_EssT/Ex_Files_Python_EssT/Exercise Files/Chap03/jackv6A_2.py", line 39, in <module>
        df['Roll'] = df['Roll'].abs()
    
      File "C:\Users\Jack\AppData\Local\Programs\Python\Python39\lib\site-packages\pandas\core\generic.py", line 9773, in abs
        return np.abs(self)  # type: ignore[return-value]
    
      File "C:\Users\Jack\AppData\Local\Programs\Python\Python39\lib\site-packages\pandas\core\generic.py", line 2032, in __array_ufunc__
        return arraylike.array_ufunc(self, ufunc, method, *inputs, **kwargs)
    
      File "C:\Users\Jack\AppData\Local\Programs\Python\Python39\lib\site-packages\pandas\core\arraylike.py", line 364, in array_ufunc
        result = getattr(ufunc, method)(*inputs, **kwargs)
    
    TypeError: bad operand type for abs(): 'str'

---
+++ jackv6A_3.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 18:40:36 2021
+@author: Jack
+"""
+
+import pandas as pd
+import numpy as np
+# from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+# Open an existing workbook:
+# wb = load_workbook('D:/DataFileForSeismometer/Seismograph_Data.xlsx')
+# wb = load_workbook('./Seismograph_Data.xlsx')
+
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html
+# Crate a new notebook
+wb = Workbook()
+# A workbook is always created with at least one worksheet. You can get it by using the Workbook.active property:
+ws = wb.active
+# Name worksheet 0
+ws.title = "My seismic data"
+
+# exit()
+
+# https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+# dtypeType name or dict of column -> type, optional
+# Data type for data or columns. E.g. {‘a’: np.float64, ‘b’: np.int32, ‘c’: ‘Int64’} Use str or object
+# together with suitable na_values settings to preserve and not interpret dtype. If converters are specified,
+# they will be applied INSTEAD of dtype conversion.
+col_names = ['Date Time', 'Roll', 'Pitch', 'Direction Angle', 'Temperature']
+df = pd.read_csv('D:/DataFileForSeismometer/DATALOG1.csv', names=col_names, low_memory=False, encoding='utf-8', sep='\s+')
+
+# ****     the sep='\s+' I got from an internet reasearch and it seem to take care of the format issues    *******
+
+print(df)
+
+# https://stackoverflow.com/questions/34962104/how-can-i-use-the-apply-function-for-a-single-column
+df['Roll'] = df['Roll'].abs()
+df['Pitch'] = df['Pitch'].abs()
+
+# Print elements 1 to 5 only.
+print(df.iloc[1:5])
+
+# To convert a dataframe into a worksheet
+# https://openpyxl.readthedocs.io/en/stable/pandas.html
+for row in dataframe_to_rows(df, index=False, header=True):
+    ws.append(row)
+
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html#saving-to-a-file
+wb.save('Seismograph_Data.xlsx')
